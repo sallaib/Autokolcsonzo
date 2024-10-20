84b882945fc0310e837690bf95106dd227c0b93b
bence <sallai97@gmail.com>
2024-10-20 14:09:05 +0200

    first commit

---
+++ auto.py
@@ -0,0 +1,12 @@
+# auto.py aa
+from abc import ABC, abstractmethod
+
+class Auto(ABC):
+    def __init__(self, rendszam, tipus, berleti_dij):
+        self.rendszam = rendszam
+        self.tipus = tipus
+        self.berleti_dij = berleti_dij
+
+    @abstractmethod
+    def info(self):
+        pass
